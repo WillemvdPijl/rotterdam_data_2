b56510d2546b924e024730efd00becf1805e75f9
Vitrav <dave.visser@outlook.com>
2016-06-28 15:29:33 +0200

    Werkende CSV reader

---
+++ app/src/main/java/topkek_mobile/fragments1/CSVFile_Test.java
@@ -0,0 +1,34 @@
+package topkek_mobile.fragments1;
+
+import android.app.Activity;
+import android.os.Parcelable;
+import android.os.Bundle;
+import android.widget.ListView;
+import java.io.InputStream;
+import java.util.List;
+
+
+public class CSVFile_Test extends Activity {
+    private ListView listView;
+    private ItemArrayAdapter itemArrayAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_csvfile__test);
+        listView = (ListView) findViewById(R.id.listView);
+        itemArrayAdapter = new ItemArrayAdapter(getApplicationContext(), R.layout.item_layout);
+
+        Parcelable state = listView.onSaveInstanceState();
+        listView.setAdapter(itemArrayAdapter);
+        listView.onRestoreInstanceState(state);
+
+        InputStream inputStream = getResources().openRawResource(R.raw.fietsdiefstal);
+        CSVFile csvFile = new CSVFile(inputStream);
+        List<String[]> scoreList = csvFile.read();
+
+        for(String[] scoreData:scoreList ) {
+            itemArrayAdapter.add(scoreData);
+        }
+    }
+}
