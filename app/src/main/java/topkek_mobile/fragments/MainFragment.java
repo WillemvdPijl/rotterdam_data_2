bec78d3b78dd1b1a72057e6a6fb0cf04997a0595
Willem_Pijl <willemvanderpijl@hotmail.com>
2016-06-28 10:57:24 +0200

    Initial commit

---
+++ app/src/main/java/topkek_mobile/fragments/MainFragment.java
@@ -0,0 +1,26 @@
+package topkek_mobile.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import topkek_mobile.fragments1.R;
+
+/**
+ * Created by Willem on 27-6-2016.
+ */
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return rootView;
+    }
+
+}
